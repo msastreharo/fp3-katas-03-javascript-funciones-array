0fc3d6afd93e15ea2d3ed0fadc56666143f32c3f
Marta <msastreharo@gmail.com>
2022-05-03 10:25:36 +0200

    Half of the solutions done

--- src/funcArray.js
+++ src/funcArray.js
@@ -1,9 +1,40 @@
 /* Encuentra el máximo */
 
-export function maxOfTwoNumbers(){}
-export function findLongestWord(){}
-export function sumArray(){}
-export function averageNumbers(){}
+export function maxOfTwoNumbers(num1, num2){
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+export function findLongestWord(array){
+    if(!array.length) {
+        return undefined;
+    }
+
+    let longestWord = "";
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].length > longestWord.length) {
+            longestWord = array[i];
+        }
+    } return longestWord;
+}
+
+export function sumArray(array){
+    let suma = 0;
+    for (let i = 0; i < array.length; i++) {
+        suma += array[i];
+    } return suma;
+}
+
+export function averageNumbers(array){
+    if (!array.length) {
+        return undefined;
+    }
+    return sumArray(array) / array.length;
+}
+
 export function averageWordLength(){}
 export function uniquifyArray(){}
 export function doesWordExist(){}
